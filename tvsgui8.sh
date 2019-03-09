f9bd9bfd1db0e91d8689526c938a9f87b542ddc0
морковка а куда <45885250+tvsclass@users.noreply.github.com>
2019-03-09 19:10:45 +0300

    stable v1

--- tvsgui8.sh
+++ tvsgui8.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
-toilet tvsgui 8 --metal
-echo closed beta. 2sc
-
-#ну вот 8 версия... тут моноскрипт, это удобнее...
+#ну вот 8 версия... это удобнее...
 #сюда будет отдельный репозиторий на гитхабе.
 
 #копируем сам скрипт
 
-echo installing...
+function quest {
+echo '[question]:' $1
+read ban
+case $ban in
+y)
+echo 
+;;
+*)
+message 'отменено пользователем'
+exit 0 
+;;
+esac
+}
+
+function message {
+echo "[tvsgui] message:"  $1 ; }
+
+function list {
+echo '[list]' '[' $1 ']'  ; }
+
+
+message 'установка...'
 cp tvsgui /usr/local/bin/
 chmod +x /usr/local/bin/tvsgui
 
+echo Необходимые момпоненты:
+echo
+list 'git'
+list 'tvspkg 2.6.9-0+'
+list 'tightvncserver 1.0+'
+list 'lxpanel 1.0+'
+list 'nautilus 1.0+'
+list 'openbox 1.0+'
 #ну и запускаем установку
-
-tvsgui --install
+quest 'Установить компоненты?'
+apt install lxpanel nautilus openbox git -y >> /dev/null
+git clone https://github.com/tvsclass/tvspkg >> /dev/null
+cd tvspkg
+rm /usr/local/bin/tvsgui
+cp tvspkg /usr/bin/
+chmod +x /usr/bin/tvspkg 
+message 'Установка завершена.'
 
 #ну всё. действия этого скрипта завершены
